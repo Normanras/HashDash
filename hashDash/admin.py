df071fb4ea21ab84bdf8f00bde597b780e0d5637
Norm Rasmussen <norman.rasmussen@me.com>
2022-05-16 17:16:28 -0400

    model changes

--- hashDash/admin.py
+++ hashDash/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import creator, dashCode, dashboard
+
+admin.site.register(dashboard)
+admin.site.register(dashCode)
+admin.site.register(creator)
--- hashDash/models.py
+++ hashDash/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class dashboard(models.Model):
+    dashboard = models.ImageField() #upload_to=hashdash_pictures
+    dash_name = models.CharField(max_length=50)
+    tags = models.CharField(max_length=20)
+    description = models.TextField(max_length=250)
+
+class dashCode(models.Model):
+    dash_name = models.CharField(max_length=50)
+    yaml = models.TextField()
+
+class creator(models.Model):
+    username = models.CharField(max_length=30)
+    
+    class SocialMedia(models.TextChoices):
+        HACOMMUNITY = 'HC', ('Home Assistant Community')
+        REDDIT = 'RD', ('Reddit')
+        TWITTER = 'TW', ('Twitter')
+        FACEBOOK = 'FB', ('Facebook')
+        TIKTOK = 'TT', ('Tik-Tok')
+    
+    social_media = models.CharField(choices=SocialMedia.choices, default=None, max_length=50)
